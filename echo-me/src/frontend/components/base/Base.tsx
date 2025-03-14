7ff8c54e9a91d3417f492b988b5bb78945a9e9e6
yu_to <yuto.aka1127@gmail.com>
2025-03-14 17:59:17 +0900

    修正

--- echo-me/src/frontend/components/base/Base.tsx
+++ echo-me/src/frontend/components/base/Base.tsx
@@ -1,17 +1,18 @@
 'use client';
-import React,{useEffect, useState,useMemo} from 'react';
+
+import React, { useEffect, useState, useMemo } from 'react';
 
 const Base = () => {
     const [count, setCount] = useState(0);
+    
     return (
         <div>
-        <h2>count: {count}</h2>
-        <button
-        onclick={() => setCount(count + 1)}>
-            クリック
-        </button>
+            <h2>count: {count}</h2>
+            <button onClick={() => setCount(count + 1)}>
+                クリック
+            </button>
         </div>
-    )
-}
+    );
+};
 
 export default Base;
