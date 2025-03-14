fd1855ff453d077bffdb58fb913293c759264327
yu_to <yuto.aka1127@gmail.com>
2025-03-14 17:56:22 +0900

    a

--- echo-me/src/index.js
+++ echo-me/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Base from './frontend/components/base/Base';
+import Base from './frontend/components/base';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
